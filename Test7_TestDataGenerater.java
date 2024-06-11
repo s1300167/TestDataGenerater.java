3baec1ca89128565aafc216d47a531dc24aca242
s1300167 <136040755+s1300167@users.noreply.github.com>
2024-06-11 21:23:41 +0900

    Update and rename TestDataGenerater.java to Test7_TestDataGenerater.java

--- Test7_TestDataGenerater.java
+++ Test7_TestDataGenerater.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class TestDataGenerater {
+public class Test7_TestDataGenerater {
 	
 	private static final Random random = new Random();
 	
